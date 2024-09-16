acf425c1f455cbcd83c6124baa47a84e82e23364
Jerry Hobby <jerry@jerryhobby.com>
2024-09-16 12:23:36 -0500

    test3

--- scratch.py
+++ scratch.py
@@ -2,6 +2,7 @@ def books():
     return "0.Fantasy, 1.Science_Fiction, 2.Historical_Fiction, 3.Mystery, 4.Horror, 5.Romance_Novel, 6.Literary_Fiction, 7.Memoir, 8.Thriller, 9.Adventure_Fiction, 10.Magical_Realism, 11.Children's_Literature, 12.Contemporary_Fantasy, 13.Dystopian_Fiction, 14.Young_Adult, 15.Fairytale, 16.Fiction, 17.Romantic_Suspense, 18.Women's_Fiction, 19.Graphic_Novels, 20.History, 21.Short_Story, 22.Crime_Fiction, 23.Erotic_Romance_Novels"
 
 print("Are you a BOOKS/NOVELS person? TRUE or FALSE!")
+
 ans1 = str(input())
 print( ans1.capitalize() )
 
